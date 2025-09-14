d14c2b5df1a2bc3094cb8508f6c4cc01a6a66586
Chien <vumanhchien511203@gmail.com>
2025-09-14 13:31:33 +0700

    first demo

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage(type) {
+  if (type === 'Lời chúc') {
+    alert('14/9/2025 - Chúc mừng bạn chính thức tốt nghiệp. Bây giờ sẽ là một hành trình mới. Chúc bạn sẽ luôn gặp may mắn trong công việc và cả cuộc sống nhá');
+  } else if (type === 'Kỷ niệm') {
+    alert('Bản project phụ đơn giản hơn nên không tích hợp phần viết thêm được. Thôi thì chúc bạn tốt nghiệp vui vẻ kekeke');
+  }
+}
