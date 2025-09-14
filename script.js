6709d785e1f858a05d8f68d9b8d169547eb2cb36
Chien <vumanhchien511203@gmail.com>
2025-09-14 13:41:43 +0700

    nothing much

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function showMessage(type) {
   if (type === 'Lời chúc') {
-    alert('14/9/2025 - Chúc mừng bạn chính thức tốt nghiệp. Bây giờ sẽ là một hành trình mới. Chúc bạn sẽ luôn gặp may mắn trong công việc và cả cuộc sống nhá');
+    showWishesDialog();
   } else if (type === 'Kỷ niệm') {
     alert('Bản project phụ đơn giản hơn nên không tích hợp phần viết thêm được. Thôi thì chúc bạn tốt nghiệp vui vẻ kekeke');
   }
 }
+
+function showWishesDialog() {
+  document.getElementById('wishesDialog').style.display = 'flex';
+}
+
+function closeWishesDialog() {
+  document.getElementById('wishesDialog').style.display = 'none';
+}
+
